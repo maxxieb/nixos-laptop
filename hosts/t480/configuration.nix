3858e4170bf117e1c4d1817a46ae2446b1bde250
Max Brydak <maxbrydak@gmail.com>
2025-04-10 16:21:56 +0200

    add qlb

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -304,6 +304,20 @@ in {
     ];
     shell = pkgs.zsh;
   };
+  users.users.qlb = {
+    initialPassword = "changethispassword";
+    isNormalUser = true;
+    description = "qlb";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+      "scanner"
+      "lp"
+      "libvirtd"
+    ];
+    shell = pkgs.zsh;
+  };
   nixpkgs.config.permittedInsecurePackages = ["electron-25.9.0"];
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
