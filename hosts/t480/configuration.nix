2c508aa56ba37f6967b34c9b6f121a54a3f5294c
mbrydak <maxbrydak@gmail.com>
2025-06-14 02:53:20 +0200

    add vscode with remote session

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -62,6 +62,20 @@ in {
       xterm
       rofi
       cloudflare-warp
+      (vscode-with-extensions.override {
+        vscodeExtensions = with vscode-extensions; [
+          bbenoist.nix
+          ms-python.python
+          ms-vscode-remote.remote-ssh
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "remote-ssh-edit";
+            publisher = "ms-vscode-remote";
+            version = "0.47.2";
+            sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+          }
+        ];
+      })
     ];
   };
   boot = {
