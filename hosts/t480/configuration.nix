237606abc1c87ced4daf0755af883fdbecf43d30
mbrydak <maxbrydak@gmail.com>
2025-06-14 03:36:44 +0200

    update nix version from 24.11 to 25.05

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -359,7 +359,7 @@ in {
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
   systemd.packages = [ pkgs.cloudflare-warp ]; # for warp-cli
   systemd.targets.multi-user.wants = [ "warp-svc.service" ]; # causes warp-svc to be started automatically
 }
