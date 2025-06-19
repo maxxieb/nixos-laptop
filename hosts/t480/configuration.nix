6d3a20109c90d73ee9fc26a1f9b030030c294260
mbrydak <maxbrydak@gmail.com>
2025-06-19 02:32:42 +0200

    fix helix

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -55,7 +55,6 @@ in {
     # List packages installed in system profile. To search, run:
     # $ nix search wget
     systemPackages = with pkgs; [
-      helix
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
       mako
@@ -274,29 +273,7 @@ in {
 
     # enalbe zsh
     zsh.enable = true;
-    nix-ld.enable = true;
-    helix = {
-      enable = true;
-      settings = {
-        theme = "autumn_night_transparent";
-        editor.cursor-shape = {
-          normal = "block";
-          insert = "bar";
-          select = "underline";
-        };
-      };
-      languages.language = [{
-        name = "nix";
-        auto-format = true;
-        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-      }];
-      themes = {
-        autumn_night_transparent = {
-          "inherits" = "autumn_night";
-          "ui.background" = { };
-        };
-      };
-    };
+    nix-ld.enable = true;    
   };
 
   # Configure console keymap
